a5c57d61c34e18d7860edbdf12fb05d697caf141
keitagsp <keitagsp@me.com>
2021-04-28 17:23:56 +0900

    20210428

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'usestrict';
+
+{
+const hidden_news = document.getElementById('hidden_news');
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => { hidden_news.style.display === 'none' ? hidden_news.style.display = 'block' : hidden_news.style.display = 'none'; });
+}
