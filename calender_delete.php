391a41f14b2b36f0ef9f94e6a6af42a9c9046621
keitagsp <83049576+keitagsp@users.noreply.github.com>
2021-08-01 08:24:14 +0900

    2nd

--- calender_delete.php
+++ calender_delete.php
@@ -1,16 +1,16 @@
 <?php
-// var_dump($_GET);
-// exit();
+session_start();
+include("functions.php");
+check_session_id();
 
-$id = $_GET['id'];
+$id = $_GET["id"];
 
-include('functions.php');
 $pdo = connect_to_db();
 
-$sql = 'DELETE FROM recordbag WHERE id=:id';
+$sql = "DELETE FROM todo_table WHERE id=:id";
 
 $stmt = $pdo->prepare($sql);
-$stmt->bindValue(':id', $id, PDO::PARAM_STR);
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $status = $stmt->execute();
 
 if ($status == false) {
@@ -18,7 +18,6 @@ if ($status == false) {
     echo json_encode(["error_msg" => "{$error[2]}"]);
     exit();
 } else {
-    header("Location:recordbag_read.php");
-    // var_dump($record);
+    header("Location:todo_read.php");
     exit();
 }
--- functions.php
+++ functions.php
@@ -2,14 +2,27 @@
 
 function connect_to_db()
 {
-$dbn = 'mysql:dbname=gsacf_l05_08;charset=utf8;port=3306;host=localhost';
-$user = 'root';
-$pwd = '';
+    $dbn = 'mysql:dbname=yoru;charset=utf8;port=3306;host=localhost';
+    $user = 'root';
+    $pwd = '';
 
-try {
-    return new PDO($dbn, $user, $pwd);
-} catch (PDOException $e) {
-    echo json_encode(["db error" => "{$e->getMessage()}"]);
-    exit();
+    try {
+        return new PDO($dbn, $user, $pwd);
+    } catch (PDOException $e) {
+        echo json_encode(["db error" => "{$e->getMessage()}"]);
+        exit();
+    }
 }
-};
+
+function check_session_id()
+{
+    if (
+        !isset($_SESSION["session_id"]) ||
+        $_SESSION["session_id"] != session_id()
+    ) {
+        header("Location:todo_login.php");
+    } else {
+        session_regenerate_id(true);
+        $_SESSION["session_id"] = session_id();
+    }
+}
