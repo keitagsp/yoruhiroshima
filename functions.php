b7efe451eb80616495ef76c1533c733dff462f97
keitagsp <83049576+keitagsp@users.noreply.github.com>
2021-09-16 04:04:48 +0900

    commit

--- functions.php
+++ functions.php
@@ -1,13 +1,18 @@
 <?php
 
-// try {
-//     $db = new PDO('mysql:dbname=heroku_c1e6e970159724e;host=us-cdbr-east-04.cleardb.com;charset=utf8', 'baed42e817e38d', 'd14966da');
-// } catch (PDOException $e) {
-//     print('DB接続エラー:' . $e->getMessage());
+try {
+    $db = new PDO('mysql:dbname=heroku_c1e6e970159724e;host=us-cdbr-east-04.cleardb.com;charset=utf8', 'baed42e817e38d', 'd14966da');
+} catch (PDOException $e) {
+    print('DB接続エラー:' . $e->getMessage());
+}
 
 
 function connect_to_db()
 {
+    $dbn = 'mysql:dbname=yoru;charset=utf8;port=3306;host=localhost';
+    $user = 'root';
+    $pwd = '';
+
     try {
         $db = new PDO('mysql:dbname=heroku_c1e6e970159724e;host=us-cdbr-east-04.cleardb.com;charset=utf8', 'baed42e817e38d', 'd14966da');
     } catch (PDOException $e) {
