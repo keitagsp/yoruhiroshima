655997b04e1dfaa906b6df3ce7c9cf9367d50d6c
keitagsp <83049576+keitagsp@users.noreply.github.com>
2021-09-19 22:15:05 +0900

    5

--- index.php
+++ index.php
@@ -8,18 +8,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- jquery--------------------------------------------------------------------------------------------------- -->
+    <!-- jquery--------------------------------------------------------------------------------------------------- -->
 
     <script type="text/javascript" src="./js/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="slick-1.8.1/slick/slick.min.js"></script>
 
-<!-- フォント--------------------------------------------------------------------------------------------------- -->
+    <!-- フォント--------------------------------------------------------------------------------------------------- -->
 
-    
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/yakuhanjp@3.2.0/dist/css/yakuhanjp.min.css">
 
     <link rel="stylesheet" href="https://use.typekit.net/opa4ojb.css">
-        
+
     <!-- css--------------------------------------------------------------------------------------------------- -->
 
     <link rel="stylesheet" href="./css/main.css" media="screen and (min-width:600px)">
@@ -40,7 +40,7 @@
 <body>
 
 
-<div class="wrapper open_animation">
+    <div class="wrapper open_animation">
 
         <!-- ヘッダー -->
         <div class="header">
@@ -59,225 +59,233 @@
                     <li><a href="https://suzuri.jp/keitagsp" target="_blank" rel="noopener noreferrer">GOODS</a></li>
                     <li><a href="party_dj.html">PARTY & DJ's</a></li>
                     <li><a href="#access">ACCESS</a></li>
-                    <li><a href="login.php">LOGIN</a></li>
+                    <li><a href="login.html">LOGIN</a></li>
                 </ul>
             </div>
         </div>
-        
-        
+
+
         <!-- トップ -->
-            <div class="Top">
-                <div class="rogo"><img src="./img/yorurogo.svg" alt=""></div>
-                <div class="rogokana"><img src="./img/yorurogokana.svg" alt=""></div>                
-                <div class="tsuki"><img src="./img/tsukikuro.png" alt=""></div>
-            </div>
-        
-        
+        <div class="Top">
+            <div class="rogo"><img src="./img/yorurogo.svg" alt=""></div>
+            <div class="rogokana"><img src="./img/yorurogokana.svg" alt=""></div>
+            <div class="tsuki"><img src="./img/tsukikuro.png" alt=""></div>
+        </div>
+
+
         <!-- pickup -->
         <div class="pickup animation">
-                <div class="border_anime">
-                    <p>PICK UP</p>  
+            <div class="border_anime">
+                <p>PICK UP</p>
+            </div>
+
+            <div class="horizontal_list">
+                <div class="Party">
+                    <div class="datetime">
+                        <p>04.29.THU</p>
+                    </div>
+                    <div class="flyer">
+                        <img src="./img/0429auditorylounge.jpg" alt="">
+                    </div>
+
+                    <div class="date">
+                        <p class="Party_title">AUDITORY LOUNGE</p>
+                        <div class="dj">
+                            <p>YASU, GAKU</p>
+                        </div>
+                    </div>
+
                 </div>
-        
-                <div class="horizontal_list" >
-                        <div class="Party">
-                            <div class="datetime">
-                                <p>04.29.THU</p>
-                            </div>
-                            <div class="flyer">
-                                <img src="./img/0429auditorylounge.jpg" alt="">
-                            </div>
-                            
-                            <div class="date">
-                                <p class="Party_title">AUDITORY LOUNGE</p>        
-                                <div class="dj">
-                                    <p>YASU, GAKU</p>
-                                </div>
-                            </div>
-                            
+
+                <div class="Party">
+                    <div class="datetime">
+                        <p>04.02.FRI</p>
+                    </div>
+                    <div class="flyer">
+                        <img src="./img/0402rondas.jpg" alt="">
+                    </div>
+                    <div class="date">
+                        <p class="Party_title">ROMNDAS</p>
+
+                        <div class="dj">
+                            <p>TSUJI</p>
                         </div>
-                    
-                        <div class="Party">
-                            <div class="datetime">
-                                <p>04.02.FRI</p>
-                            </div>
-                            <div class="flyer">
-                                <img src="./img/0402rondas.jpg" alt="">
-                            </div>
-                            <div class="date">
-                                <p class="Party_title">ROMNDAS</p>
-                        
-                                <div class="dj">
-                                    <p>TSUJI</p>
-                                </div>
-                            </div>                      
-                        </div>
-                        <div class="Party">
-                            <div class="datetime">
-                                <p>04.18.SUN</p>
-                            </div>                       
-                            <div class="flyer">
-                                <img src="./img/0418meltingpot.jpg" alt="">
-                            </div>
-                            <div class="date">
-                                <p class="Party_title">MELTING POT</p>
-                                <div class="dj">
-                                    <p>SEIJI, ONO</p>
-                                </div>
-                            </div>
-                                                
-                        </div>
-                        <div class="Party">
-                            <div class="datetime">
-                                <p>05.04.TUE</p>
-                            </div>
-                            <div class="flyer">
-                                <img src="./img/0504archiv.jpg" alt="">
-                            </div>
-                            <div class="date">
-                                <p class="Party_title">ARCHIV</p>
-                        
-                                <div class="dj">
-                                    <p>HARUKA, CHANN, OCCA</p>
-                                </div>
-                            </div>
-                                                
-                        </div>
-                        <div class="Party">
-                            <div class="datetime">
-                                <p>06.19.SAT</p>
-                            </div>
-                            <div class="flyer">
-                                <img src="./img/0619cloud9.jpg" alt="">
-                            </div>
-                            <div class="date">
-                                <p class="Party_title">CLOUD9</p>
-                        
-                                <div class="dj">
-                                    <p>KANI</p>
-                                </div>
-                            </div>
-                        
-                        </div><div class="Party">
-                            <div class="datetime">
-                                <p>7.10.SAT</p>
-                            </div>
-                            <div class="flyer">
-                                <img src="./img/anniversary_image.jpg" alt="">
-                            </div>
-                            <div class="date">
-                                <p class="Party_title">28th ANNIVESARY PARTY</p>
-                        
-                                <div class="dj">
-                                    <p>YORU ALL DJ'S</p>
-                                </div>
-                            </div>
-                        
-                        </div>
+                    </div>
                 </div>
+                <div class="Party">
+                    <div class="datetime">
+                        <p>04.18.SUN</p>
+                    </div>
+                    <div class="flyer">
+                        <img src="./img/0418meltingpot.jpg" alt="">
+                    </div>
+                    <div class="date">
+                        <p class="Party_title">MELTING POT</p>
+                        <div class="dj">
+                            <p>SEIJI, ONO</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="Party">
+                    <div class="datetime">
+                        <p>05.04.TUE</p>
+                    </div>
+                    <div class="flyer">
+                        <img src="./img/0504archiv.jpg" alt="">
+                    </div>
+                    <div class="date">
+                        <p class="Party_title">ARCHIV</p>
+
+                        <div class="dj">
+                            <p>HARUKA, CHANN, OCCA</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="Party">
+                    <div class="datetime">
+                        <p>06.19.SAT</p>
+                    </div>
+                    <div class="flyer">
+                        <img src="./img/0619cloud9.jpg" alt="">
+                    </div>
+                    <div class="date">
+                        <p class="Party_title">CLOUD9</p>
+
+                        <div class="dj">
+                            <p>KANI</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="Party">
+                    <div class="datetime">
+                        <p>7.10.SAT</p>
+                    </div>
+                    <div class="flyer">
+                        <img src="./img/anniversary_image.jpg" alt="">
+                    </div>
+                    <div class="date">
+                        <p class="Party_title">28th ANNIVESARY PARTY</p>
+
+                        <div class="dj">
+                            <p>YORU ALL DJ'S</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
 
 
         <!-- News -->
         <div class="news animation">
             <div class="border_anime">
-            <p>NEWS</p>
-        </div>
-        
-        <div class="sliderArea">
-            <div class="regular_2 slider">
-                <div>
-                    <img src="./img/swet.jpg" alt="">
-                    <p class="sliderArea_text">大好評のYoruスウェットに<br>White/Greyが追加されました。<br>数量限定ですのでお早めに！</p>
-                    <p class="sliderArea_text_date">2021.04.10</p>
-                </div>
-                <div>
-                    <img src="./img/umi.jpg" alt="">
-                    <p class="sliderArea_text">今年も守ヶ浦キャンプ場で夏フェスが決定しました！<br>前売りチケット販売中です！</p>
-                    <p class="sliderArea_text_date">2021.04.01</p>
-                </div>
-                <div>
-                    <img src="./img/ondo.jpg" alt="">
-                    <p class="sliderArea_text">ご近所さんのオンド食堂にアメリカよりJoeClluselが来日します。<br>チェックしてみてください！</p>
-                    <p class="sliderArea_text_date">2021.03.26</p>
-                </div>
-                <div>
-                    <img src="./img/ondo.jpg" alt="">
-                    <p class="sliderArea_text">ご近所さんのオンド食堂にアメリカよりJoeClluselが来日します。<br>チェックしてみてください！</p>
-                    <p class="sliderArea_text_date">2021.03.26</p>
-                </div>
-                <div>
-                    <img src="./img/ondo.jpg" alt="">
-                    <p class="sliderArea_text">ご近所さんのオンド食堂にアメリカよりJoeClluselが来日します。<br>チェックしてみてください！</p>
-                    <p class="sliderArea_text_date">2021.03.26</p>
+                <p>NEWS</p>
+            </div>
+
+            <div class="sliderArea">
+                <div class="regular_2 slider">
+                    <div>
+                        <img src="./img/swet.jpg" alt="">
+                        <p class="sliderArea_text">大好評のYoruスウェットに<br>White/Greyが追加されました。<br>数量限定ですのでお早めに！</p>
+                        <p class="sliderArea_text_date">2021.04.10</p>
+                    </div>
+                    <div>
+                        <img src="./img/umi.jpg" alt="">
+                        <p class="sliderArea_text">今年も守ヶ浦キャンプ場で夏フェスが決定しました！<br>前売りチケット販売中です！</p>
+                        <p class="sliderArea_text_date">2021.04.01</p>
+                    </div>
+                    <div>
+                        <img src="./img/ondo.jpg" alt="">
+                        <p class="sliderArea_text">ご近所さんのオンド食堂にアメリカよりJoeClluselが来日します。<br>チェックしてみてください！</p>
+                        <p class="sliderArea_text_date">2021.03.26</p>
+                    </div>
+                    <div>
+                        <img src="./img/ondo.jpg" alt="">
+                        <p class="sliderArea_text">ご近所さんのオンド食堂にアメリカよりJoeClluselが来日します。<br>チェックしてみてください！</p>
+                        <p class="sliderArea_text_date">2021.03.26</p>
+                    </div>
+                    <div>
+                        <img src="./img/ondo.jpg" alt="">
+                        <p class="sliderArea_text">ご近所さんのオンド食堂にアメリカよりJoeClluselが来日します。<br>チェックしてみてください！</p>
+                        <p class="sliderArea_text_date">2021.03.26</p>
+                    </div>
                 </div>
             </div>
         </div>
-</div>
 
-        
-            
-        
+
+
+
         <!-- 求人 -->
         <div class="wanted animation">
-                <div class="wanted_text">
-                    <p class=" border_anime">WANTED</p>
-                    <p class="wanted_text_text">
+            <div class="wanted_text">
+                <p class=" border_anime">WANTED</p>
+                <p class="wanted_text_text">
                     " 音楽を一緒に共有する場所として、常に高みを目指す "<br><br><br>
                     僕たちはこのビジョンに共感できる仲間を探しています。<br><br>
                     DJ、Bar、Lighting、デザイナーを探していますが<br>
                     自分はこんなことができるっていうのも大歓迎です。<br><br><br>
                     まずはお酒でものみながら、話してみませんか？
-                    </p>
-                    <div class="contact">
+                </p>
+                <div class="contact">
                     <a class="contact_button" href="contact_form.html" target="_blank" rel="noopener noreferrer"><span>CONTACT</span></a>
-                    </div>    
                 </div>
+            </div>
         </div>
-        
-        
-        
+
+
+
         <!-- アクセス -->
         <div class="border_anime animation">
             <p>ACCESS</p>
         </div>
         <div class="access animation" id="access">
-                Arts & Tecnology Lab , Yoru<br>
-                〒730-0028 広島県広島市中区流川町<br>
-                000-000-0000<br>
-                keita@yoru.com<br><br><br>
-                <a href="https://www.google.co.jp/maps/place/%E3%80%92730-0028+%E5%BA%83%E5%B3%B6%E7%9C%8C%E5%BA%83%E5%B3%B6%E5%B8%82%E4%B8%AD%E5%8C%BA%E6%B5%81%E5%B7%9D%E7%94%BA%EF%BC%94%E2%88%92%EF%BC%98+%E6%9D%B1%E9%82%A6%E3%83%93%E3%83%AB/@34.3903662,132.4619635,17z/data=!3m1!4b1!4m5!3m4!1s0x355aa2050a24272b:0xc05adc2c08adda8e!8m2!3d34.3903618!4d132.4641522?hl=ja" target="_blank" rel="noopener noreferrer">
-                    [GOOGLE MAP]</a>
+            Arts & Tecnology Lab , Yoru<br>
+            〒730-0028 広島県広島市中区流川町<br>
+            000-000-0000<br>
+            keita@yoru.com<br><br><br>
+            <a href="https://www.google.co.jp/maps/place/%E3%80%92730-0028+%E5%BA%83%E5%B3%B6%E7%9C%8C%E5%BA%83%E5%B3%B6%E5%B8%82%E4%B8%AD%E5%8C%BA%E6%B5%81%E5%B7%9D%E7%94%BA%EF%BC%94%E2%88%92%EF%BC%98+%E6%9D%B1%E9%82%A6%E3%83%93%E3%83%AB/@34.3903662,132.4619635,17z/data=!3m1!4b1!4m5!3m4!1s0x355aa2050a24272b:0xc05adc2c08adda8e!8m2!3d34.3903618!4d132.4641522?hl=ja" target="_blank" rel="noopener noreferrer">
+                [GOOGLE MAP]</a>
         </div>
-        
-        
-        
+
+
+
         <div class="Arts">
-                <p>COPYRIGHT (C) Arts & Tecnology Lab , Yoru. ALL RIGHTS RESERVED.</p>
-            </div>
+            <p>COPYRIGHT (C) Arts & Tecnology Lab , Yoru. ALL RIGHTS RESERVED.</p>
+        </div>
 
-</div>
+    </div>
 
 
 
-<script>
-    $(function () {
-        $('.btn-gNav').on("click", function () {
+    <script>
+        $(function() {
+            $('.btn-gNav').on("click", function() {
 
-            $(this).toggleClass('open');
-            $('.header_text').toggleClass('open');
+                $(this).toggleClass('open');
+                $('.header_text').toggleClass('open');
+            });
         });
-    });
 
-        $(function () {
-                $('.open_animation').each(function () {
-                        $(this).addClass('open_active');
-                    }
-                )});
-        
-        $(function () {
-            $(window).on('load scroll', function () {
-                $('.animation').each(function () {
+        $(function() {
+            $('.gNav-menu li a').on("click", function() {
+                $('.header_text').removeClass('open');
+            });
+        });
+
+
+        $(function() {
+            $('.open_animation').each(function() {
+                $(this).addClass('open_active');
+            })
+        });
+
+        $(function() {
+            $(window).on('load scroll', function() {
+                $('.animation').each(function() {
                     //ターゲットの位置を取得
                     var target = $(this).offset().top;
                     //スクロール量を取得
@@ -294,31 +302,25 @@
         });
 
 
-    $(function () {
-        $('.gNav-menu li a').on("click", function () {
-            $('.header_text').removeClass('open');
+        $('.slider').slick({
+            dots: true, //スライドしたのドット
+            arrows: false, //左右の矢印
+            infinite: true, //スライドのループ
+            pauseOnHover: false, //ホバーしたときにスライドを一時停止しない
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            responsive: [{
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }]
         });
-    });
-    
-
-    $('.slider').slick({
-        dots: true, //スライドしたのドット
-        arrows: false,//左右の矢印
-        infinite: true,//スライドのループ
-        pauseOnHover: false,//ホバーしたときにスライドを一時停止しない
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        responsive: [{
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-    }}]});
-
-
-</script>
+    </script>
 
 
     <script src="js/main.js"></script>
 </body>
+
 </html>
